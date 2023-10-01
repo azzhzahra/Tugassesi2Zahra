361fe4f1950abc373dda28e48d64d6755f4dc970
azzhzahra <114793003+azzhzahra@users.noreply.github.com>
2023-10-01 22:18:43 +0700

    Add files via upload

---
+++ HelloZahra.js
@@ -0,0 +1,21 @@
+const Nama = "Nurazizah zahra"
+const NIM = 20220040089
+
+// aritmatika Pada JavaScript
+
+const Angka1 = 20
+const Angka2 = 10
+
+const Tambah = Angka1 + Angka2 ;
+const Kurang = Angka1 - Angka2 ;
+const Kali = Angka1 * Angka2 ;
+const Bagi = Angka1 / Angka2 ;
+
+// input variabel
+
+console.log(`Nama Saya ` +  Nama)
+console.log(`NIM Saya `  +  NIM)
+console.log("Pertambahan :  " + Tambah)
+console.log("Perkurangan :  " + Kurang)
+console.log("Perkalian :    " + Kali)
+console.log("Perbagian :    " + Bagi)
---
+++ index.js
@@ -0,0 +1,8 @@
+let firstName = "Nurazizah";
+let lastName = "Zahra";
+let age = 19;
+let isMarried = false;
+
+console.log(`Nama Lengkap : ${firstName} ${lastName}`);
+console.log(`Umur : ${age}`);
+console.log(`Is Married : ${isMarried}`);
